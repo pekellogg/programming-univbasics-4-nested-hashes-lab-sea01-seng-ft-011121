baa2b66b7973c11f0f9c2077ff40deb77443fda7
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-02 01:49:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -100,9 +100,6 @@ def changing_alan
 end
 
 def adding_to_dennis
-  # add "Assembly" to Dennis Ritchie's languages array
-  # return the entire updated hash
-
 	programmer_hash =
  		{
       :grace_hopper => {
@@ -118,5 +115,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+		programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+		programmer_hash
 end
