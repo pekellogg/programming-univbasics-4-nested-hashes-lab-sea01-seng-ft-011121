9efdef7c0e824b07be305bb6a24f1a977b101ff6
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-02 00:59:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-	programmer_hash[:dennis_ritchie][:languages][0]
+	programmer_hash[:dennis_ritchie][:languages]
 end
 
 def changing_alan
